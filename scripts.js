4bc506dfa43267a45dfde83f21609b010b744577
Lucidifier <dragan.hardi3@gmail.com>
2023-08-18 10:29:52 +0200

    add random color on mouse event

--- scripts.js
+++ scripts.js
@@ -13,22 +13,35 @@ console.log(n);
 //CREATE GRID
 function createGrid () {
 	let divContainerItem = document.querySelector('.container-div');
-	for (let i = 0; i < userGridSize*userGridSize; i++) {
+	for (let i = 0; i < userGridSize * userGridSize; i++) {
 		let tempDiv =	document.createElement('div');
 		tempDiv.classList.toggle('sketch-box');
 		tempDiv.style.width = (n);
 		tempDiv.style.height = (n);
 		divContainerItem.appendChild(tempDiv);
 
+		let randomColor;
+		function getRandomColor() {
+			function getRandomRGB () {
+				return Math.round(0 + Math.random() * (255-0));
+			}
+			randomColor = 'rgb(' + getRandomRGB() + ', ' + getRandomRGB() + ', ' + getRandomRGB() + ')';
+			console.log(randomColor); 
+			return randomColor;
+		}
+		getRandomColor();
+		console.log(randomColor); 
+
 		//HOVER EFFECT
 		function changeColorHover(box) {
-		box.addEventListener('mouseenter', () => {box.style.backgroundColor = 'black'})
-		}
+		box.addEventListener('mouseenter', () => {box.style.backgroundColor = randomColor})};
+		
 		changeColorHover(tempDiv);
 	}
-	
-	
 }
+
+//RANDOM COLOR GENERATOR
+
 
 
 createGrid();
