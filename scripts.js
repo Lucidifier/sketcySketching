824505aabc2eb017a0f53860d9d7f910792db1e1
Lucidifier <dragan.hardi3@gmail.com>
2023-08-21 23:35:45 +0200

    add grid size  button functionality, add starting 16x16 grid function

--- scripts.js
+++ scripts.js
@@ -1,19 +1,10 @@
-//GET  USER GRID SIZE
-function getUserGrid() {
-	let userGrid = prompt('Enter grid size');
-	return userGrid;
-}
-
-let userGridSize = getUserGrid();
-console.log(userGridSize);
-
-let n = ((600 / userGridSize)-2) + 'px';
-console.log(n);
-
-//CREATE GRID
-function createGrid () {
+function sketch() {
 	let divContainerItem = document.querySelector('.grid-container');
-	for (let i = 0; i < userGridSize * userGridSize; i++) {
+
+	function createGrid16x16() {
+	let n = ((600 / 16)-2) + 'px';
+	for (let i = 0; i < 16 * 16; i++) {
+
 		let tempDiv =	document.createElement('div');
 		tempDiv.classList.toggle('sketch-box');
 		tempDiv.style.width = (n);
@@ -21,27 +12,78 @@ function createGrid () {
 		divContainerItem.appendChild(tempDiv);
 
 		//RANDOM COLOR GENERATOR
-		let randomColor;
 		function getRandomColor() {
+			let randomColor;
 			function getRandomRGB () {
 				return Math.round(0 + Math.random() * (255-0));
 			}
 			randomColor = 'rgb(' + getRandomRGB() + ', ' + getRandomRGB() + ', ' + getRandomRGB() + ')';
 			return randomColor;
 		}
-		getRandomColor();
 
 		//HOVER EFFECT
-		function changeColorHover(box) {
-		box.addEventListener('mouseenter', () => {box.style.backgroundColor = randomColor})};
-		
-		changeColorHover(tempDiv);
-	}
+		function changeColorRandom(box) {
+			 box.addEventListener('mouseenter', () => {box.style.backgroundColor = getRandomColor()});
+		}
+			changeColorRandom(tempDiv);
+			}
+			
 }
 
+//GET  USER GRID SIZE
+function getUserGrid() {
+	let userGrid = prompt('Enter grid size');
+	return userGrid;
+}
 
+// let userGridSize = getUserGrid();
+// console.log(userGridSize);
 
+// let n = ((600 / userGridSize)-2) + 'px';
+// console.log(n);
 
+//CREATE GRID
+function createGrid () {
 
-createGrid();
+	let divContainerItem = document.querySelector('.grid-container');
+	let userGridSize = getUserGrid();
+	let n = ((600 / userGridSize)-2) + 'px';
 
+	for (let i = 0; i < userGridSize * userGridSize; i++) {
+
+		let tempDiv =	document.createElement('div');
+		tempDiv.classList.toggle('sketch-box');
+		tempDiv.style.width = (n);
+		tempDiv.style.height = (n);
+		divContainerItem.appendChild(tempDiv);
+
+		//RANDOM COLOR GENERATOR
+		function getRandomColor() {
+			let randomColor;
+			function getRandomRGB () {
+				return Math.round(0 + Math.random() * (255-0));
+			}
+			randomColor = 'rgb(' + getRandomRGB() + ', ' + getRandomRGB() + ', ' + getRandomRGB() + ')';
+			return randomColor;
+		}
+
+		//HOVER EFFECT
+		function changeColorRandom(box) {
+			 box.addEventListener('mouseenter', () => {box.style.backgroundColor = getRandomColor()});
+		}
+			changeColorRandom(tempDiv);
+			}
+			
+		}
+
+		//GRID SIZE BUTTON
+		let gridSizeButton = document.querySelector('.grid-size-btn');
+		gridSizeButton.addEventListener('click', ()=> {
+			createGrid();
+		} 
+		)
+		
+		createGrid16x16();
+	}
+
+// createGrid();
