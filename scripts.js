7cb5ad33ace4da425f0ce5e15b2b42ca4fe4f84d
Lucidifier <dragan.hardi3@gmail.com>
2023-08-22 00:46:08 +0200

    add an old grid remove function to grid size button so it can properly display a new grid

--- scripts.js
+++ scripts.js
@@ -1,34 +1,44 @@
 function sketch() {
 	let divContainerItem = document.querySelector('.grid-container');
+	let tempDiv;
 
-	function createGrid16x16() {
-	let n = ((600 / 16)-2) + 'px';
-	for (let i = 0; i < 16 * 16; i++) {
-
-		let tempDiv =	document.createElement('div');
-		tempDiv.classList.toggle('sketch-box');
-		tempDiv.style.width = (n);
-		tempDiv.style.height = (n);
-		divContainerItem.appendChild(tempDiv);
-
-		//RANDOM COLOR GENERATOR
-		function getRandomColor() {
-			let randomColor;
-			function getRandomRGB () {
-				return Math.round(0 + Math.random() * (255-0));
-			}
-			randomColor = 'rgb(' + getRandomRGB() + ', ' + getRandomRGB() + ', ' + getRandomRGB() + ')';
-			return randomColor;
-		}
-
-		//HOVER EFFECT
-		function changeColorRandom(box) {
-			 box.addEventListener('mouseenter', () => {box.style.backgroundColor = getRandomColor()});
-		}
-			changeColorRandom(tempDiv);
-			}
-			
+//RANDOM COLOR GENERATOR
+function getRandomColor() {
+	let randomColor;
+	function getRandomRGB () {
+		return Math.round(0 + Math.random() * (255-0));
+	}
+	randomColor = 'rgb(' + getRandomRGB() + ', ' + getRandomRGB() + ', ' + getRandomRGB() + ')';
+	return randomColor;
 }
+
+	//HOVER EFFECT
+	function changeColorRandom(box) {
+		box.addEventListener('mouseenter', () => {box.style.backgroundColor = getRandomColor()});
+ 	}
+
+ 	//CREATE STARTING 16X16 GRID
+	function createGrid16x16() {
+	let n1 = ((600 / 16)-2) + 'px';
+	for (let i = 0; i < 16 * 16; i++) {
+		tempDiv =	document.createElement('div');
+		tempDiv.classList.toggle('sketch-box');
+		tempDiv.style.width = (n1);
+		tempDiv.style.height = (n1);
+		divContainerItem.appendChild(tempDiv);
+		changeColorRandom(tempDiv);
+		}
+	}
+
+			//GRID SIZE BUTTON
+			let gridSizeButton = document.querySelector('.grid-size-btn');
+			gridSizeButton.addEventListener('click', ()=> {
+				let tempDivNodeList = document.querySelectorAll('.sketch-box');
+				for (let j=0; j<tempDivNodeList.length; j++) {
+					tempDivNodeList[j].style.display = ('none');
+				}
+				createGrid();
+			})
 
 //GET  USER GRID SIZE
 function getUserGrid() {
@@ -36,54 +46,36 @@ function getUserGrid() {
 	return userGrid;
 }
 
-// let userGridSize = getUserGrid();
-// console.log(userGridSize);
 
-// let n = ((600 / userGridSize)-2) + 'px';
-// console.log(n);
-
-//CREATE GRID
+//CREATE USER GRID
 function createGrid () {
 
-	let divContainerItem = document.querySelector('.grid-container');
 	let userGridSize = getUserGrid();
-	let n = ((600 / userGridSize)-2) + 'px';
+	let n2 = ((600 / userGridSize)-2) + 'px';
 
 	for (let i = 0; i < userGridSize * userGridSize; i++) {
-
-		let tempDiv =	document.createElement('div');
+		tempDiv =	document.createElement('div');
 		tempDiv.classList.toggle('sketch-box');
-		tempDiv.style.width = (n);
-		tempDiv.style.height = (n);
+		tempDiv.style.width = (n2);
+		tempDiv.style.height = (n2);
 		divContainerItem.appendChild(tempDiv);
 
-		//RANDOM COLOR GENERATOR
-		function getRandomColor() {
-			let randomColor;
-			function getRandomRGB () {
-				return Math.round(0 + Math.random() * (255-0));
-			}
-			randomColor = 'rgb(' + getRandomRGB() + ', ' + getRandomRGB() + ', ' + getRandomRGB() + ')';
-			return randomColor;
-		}
-
-		//HOVER EFFECT
-		function changeColorRandom(box) {
-			 box.addEventListener('mouseenter', () => {box.style.backgroundColor = getRandomColor()});
-		}
-			changeColorRandom(tempDiv);
-			}
+		changeColorRandom(tempDiv);
+	}
 			
-		}
+	}
 
-		//GRID SIZE BUTTON
-		let gridSizeButton = document.querySelector('.grid-size-btn');
-		gridSizeButton.addEventListener('click', ()=> {
-			createGrid();
-		} 
-		)
+		// //GRID SIZE BUTTON
+		// let gridSizeButton = document.querySelector('.grid-size-btn');
+		// gridSizeButton.addEventListener('click', ()=> {
+		// 	tempDiv.remove();
+		// 	// createGrid();
+		// 	} 
+		// )
 		
 		createGrid16x16();
 	}
+
+	sketch();
 
 // createGrid();
